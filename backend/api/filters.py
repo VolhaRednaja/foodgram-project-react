fa0e32e798fc0dfbdde948ab7e11a34dd6d013da
VolhaRednaja <elizaveta1421@gmail.com>
2023-08-12 05:32:45 +0300

    fifth attempt to reveiw

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -41,11 +41,11 @@ class RecipeFilter(FilterSet):
     def get_is_favorited(self, queryset, name, value):
         user = self.request.user
         if value:
-            return queryset.filter(is_favorited__user=user)
+            return queryset.filter(favorite__user=user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
         if value:
-            return queryset.filter(is_in_shopping_cart__user=user)
+            return queryset.filter(shopping_list__user=user)
         return queryset
