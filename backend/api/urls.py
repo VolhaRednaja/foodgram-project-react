44ead640b15829c368be60f3a3967afb0b61ac32
VolhaRednaja <elizaveta1421@gmail.com>
2023-08-17 01:50:23 +0300

    fifth attept to review

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
          ),
     path('', include(router.urls)),
     path('auth/token/login/', TokenCreateView.as_view(), name='login'),
-    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),    
+    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
     path(r'auth/', include('djoser.urls.authtoken')),
 ]
