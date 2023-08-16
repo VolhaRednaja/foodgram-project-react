e8fe4930a7d9e76ad89eaf1505ea9b1862cb9456
VolhaRednaja <elizaveta1421@gmail.com>
2023-08-16 22:59:20 +0300

    fifth attempt to review

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -41,10 +41,14 @@ class RecipeFilter(FilterSet):
 
     def get_favorite(self, queryset, name, item_value):
         if self.request.user.is_authenticated and item_value:
-            queryset = Recipe.objects.filter(in_favorite__user=self.request.user)
+            queryset = Recipe.objects.filter(
+                in_favorite__user=self.request.user
+                )
         return queryset
 
     def get_shopping(self, queryset, name, item_value):
         if self.request.user.is_authenticated and item_value:
-            queryset = Recipe.objects.filter(shopping_cart__user=self.request.user)
+            queryset = Recipe.objects.filter(
+                shopping_cart__user=self.request.user
+                )
         return queryset
