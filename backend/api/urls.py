e3402998615cebc7c0d58c827ab6374682077660
VolhaRednaja <elizaveta1421@gmail.com>
2023-08-18 02:30:21 +0300

    six attempt to review

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
+from djoser.views import TokenCreateView, TokenDestroyView
 from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientsViewSet, RecipeViewSet,
-                    TagsViewSet)
+                    TagsViewSet, FollowApiView, ListFollowViewSet)
 
 router = DefaultRouter()
 
@@ -11,5 +12,16 @@ router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagsViewSet, basename='tags')
 
 urlpatterns = [
+    path('auth/token/login/', TokenCreateView.as_view(), name='login'),
+    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
+
+    path('users/<int:id>/subscribe/',
+         FollowApiView.as_view(),
+         name='subscribe'
+         ),
+    path('users/subscriptions/', ListFollowViewSet.as_view(),
+         name='subscription'
+         ),
+    path('', include('djoser.urls')),
     path('', include(router.urls)),
 ]
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -3,14 +3,9 @@ from django.urls import include, path
 
 from .yasg import urlpatterns as doc_urls
 
-api_patterns = [
-    path('', include('api.urls')),
-    path('', include('users.urls'))
-]
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(api_patterns)),
+    path('api/', include('api.urls')),
     path('api/', include('rest_framework.urls'))
 ]
 
