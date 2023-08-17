715d558648c244fe2425a9ae22603d26492c5427
VolhaRednaja <elizaveta1421@gmail.com>
2023-08-17 23:42:56 +0300

    six attempt to review

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,6 +14,9 @@ router.register('tags', TagsViewSet, basename='tags')
 router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('auth/token/login/', TokenCreateView.as_view(), name='login'),
+    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
+    path('auth/', include('djoser.urls.authtoken')),
     path('users/<int:id>/subscribe/',
          FollowApiView.as_view(),
          name='subscribe'
@@ -21,8 +24,5 @@ urlpatterns = [
     path('users/subscriptions/', ListFollowViewSet.as_view(),
          name='subscription'
          ),
-    path('auth/token/login/', TokenCreateView.as_view(), name='login'),
-    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
-    path('auth/', include('djoser.urls.authtoken')),
     path('', include(router.urls)),
 ]
